23093f58eccafd6396cedbd677627b94361e8990
makar <makar.peter98@gmail.com>
2024-12-03 08:55:57 +0100

    Commit Gergőnek speciálba

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,5 +3,25 @@ import axios from "axios";
 
 export default function Navbar()
 {
-	return (<div>Navbar</div>)
+	return (
+	<div>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+		  <div className="container-fluid">
+			<a className="navbar-brand" href="#">Person manager</a>
+			<button 
+				className="navbar-toggler" 
+				type="button" 
+				data-bs-toggle="collapse" 
+				data-bs-target="#navbarSupportedContent" 
+				aria-controls="navbarSupportedContent" 
+				aria-expanded="false" 
+				aria-label="Toggle navigation"
+			>
+			  <span className="navbar-toggler-icon"></span>
+			</button>
+			
+			<button className="btn btn-outline-light">Add new person</button>
+		  </div>
+		</nav>
+	</div>)
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,5 +3,23 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
-  return (<div>Home</div>);
+	const [persons, setpersons] = useState([]);
+	const { id } = useParams();
+	
+	useEffect(() => {
+		loadpersons();
+	}, []);
+	
+	const loadpersons = async () => {
+		const result = await axios.get("http://localhost:8080/persons");
+		console.log(result.data);
+		setpersons(result.data);
+	};
+	
+	const deletePerson = async (id) => {
+		await axios.delete(`http://localhost:8080/person/${id}`);
+		loadpersons();
+	};
+	
+	return (0);
 }
