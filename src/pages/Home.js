d30e39be2cceab4dcc07a64ce0686b81daa1627e
makar <makar.peter98@gmail.com>
2024-12-03 09:13:49 +0100

    Frontend kész

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,5 +21,51 @@ export default function Home() {
 		loadpersons();
 	};
 	
-	return (0);
+	return (<div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Név</th>
+              <th scope="col">Felhasználónév</th>
+              <th scope="col">Email</th>
+              <th scope="col">Művelet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {persons.map((Person, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{Person.name}</td>
+                <td>{Person.username}</td>
+                <td>{Person.email}</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                    to={`/viewPerson/${Person.id}`}
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/editPerson/${Person.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deletePerson(Person.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>);
 }
